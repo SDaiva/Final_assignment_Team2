297baaec4d2f48b26bf5364666eeb96a65645d59
DianaPavulane <125908449+DianaPavulane@users.noreply.github.com>
2023-04-19 20:33:21 +0300

    Update main.tf
    Changed the port

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "aws_security_group" "team2_final_assignment_security_group" {
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = 8000
+    to_port = 8000
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
