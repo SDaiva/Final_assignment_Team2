d91638933e5fb742c606789745b34e08051538e7
SDaiva <125908536+SDaiva@users.noreply.github.com>
2023-04-22 15:31:50 +0300

    Update run.py
    Commit Update run.py

--- docker/run.py
+++ docker/run.py
@@ -1,43 +1,7 @@
-import os
-import time
-import mysql.connector
-import mysql.connector
-from decouple import config
+import connexion
 
-
-def connect_to_mysql():
-    start_time = time.time()
-    timeout = 60
-
-    while time.time() - start_time < timeout:
-        try:
-            connection = mysql.connector.connect(
-                host=config('DB_HOST'),
-                user=config('DB_USER'),
-                password=config('DB_PASSWORD'),
-                database=config('DB_NAME')
-            )
-            print("Connected to MySQL")
-            return connection
-        except mysql.connector.Error as e:
-            print(f"Error connecting to MySQL: {e}")
-            time.sleep(5)
-
-    raise Exception(f"Could not connect to MySQL within {timeout} seconds")
-
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
 
 if __name__ == "__main__":
-    try:
-        while True:
-            with connect_to_mysql() as connection:
-                cursor = connection.cursor()
-                try:
-                    cursor.execute("SELECT VERSION()")
-                    result = cursor.fetchone()
-                    print(f"!!MySQL version!!: {result}")
-                finally:
-                    cursor.close()
-
-            time.sleep(5)
-    except KeyboardInterrupt:
-        print("Stopping script")
+    app.run(host="0.0.0.0", port=8000, debug=True)
